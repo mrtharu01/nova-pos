23ac6e97a9a36b4ee50f839b92e0687e221870c5
mrtharu01 <tharukmaldeniya@gmail.com>
2026-09-25 14:33:21 +0530

    Add platform dashboard route

---
+++ app/internal/[key]/(protected)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import {
+  PlatformDashboardClient,
+} from "@/components/platform/PlatformDashboardClient";
+
+
+export default function PlatformDashboardPage() {
+  return (
+    <PlatformDashboardClient />
+  );
+}
