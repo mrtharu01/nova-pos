2a4c5be384070ed4531d0dde8298370be01d8d54
mrtharu01 <tharukmaldeniya@gmail.com>
2026-09-26 14:54:12 +0530

    Retry Phase 8A preview deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import {
 } from "@/components/providers/ThemeProvider";
 
 
-  // ARC preview redeploy marker: phase8a-pack-loose-2026-09-26
+  // ARC preview redeploy marker: phase8a-pack-loose-2026-09-26-retry-2
 
 export const metadata: Metadata = {
   title: {
